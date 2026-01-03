470f7d2863f0266e1cee3bde04efe3370d61ed7d
austinetim <austinetimothyfriday@gmail.com>
2026-01-03 23:56:54 +0100

    initiate treasure map prog

---
+++ month_2/treasure_map.py
@@ -0,0 +1,26 @@
+row1 = ["⬜", "⬜", "⬜"]
+row2 = ["⬜", "⬜", "⬜"]
+row3 = ["⬜", "⬜", "⬜"]  
+
+map = [row1, row2, row3]
+
+# print(f"{row1}\n{row2}\n{row3}")
+# print(map)
+
+# lr1 = len(row1)
+# lr2 = len(row2)
+# lr3 = len(row3)
+# l_map = len(map)
+
+# print(lr1, lr2, lr3, l_map)
+
+position = input("Where do you want to put the treasure?  ")
+
+col_p = position[0]
+row_p = position[1]
+
+if position == col_p and position == row_p:
+    
+
+
+print(col_p, row_p)
