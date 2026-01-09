efbc017a4a9fc87abb1bd93e7d2a943b391ef6c4
austinetim <austinetimothyfriday@gmail.com>
2026-01-09 23:57:45 +0100

    add Reeborg's World code

---
+++ month_2/reeborg_world_code.py
@@ -0,0 +1,44 @@
+#The code below should be run in the Reeborg's World coding platform. 
+# Visit:https://reeborg.ca/reeborg.html test the code.
+
+# Code to turn the robot in Reeborg's World turn around
+
+# def turn_around():
+#    turn_left()
+#    turn_left()
+# move()
+# move()
+# turn_around()
+# move()
+# move()
+# turn_around()
+
+
+
+# #Code to turn the robot in Reeborg's World draw a square
+
+# # Function to turn the robot right
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# # Function to move the robot 4 times
+# def move_it():
+#     move()
+#     move()
+#     move()
+#     move()
+
+# # The robot's movement is maneuvered below
+# turn_left()
+# move_it()
+# turn_right()
+# move_it()
+# turn_right()
+# move_it()
+# turn_right()
+# move_it()
+# turn_right()
+# move()
+# move()
