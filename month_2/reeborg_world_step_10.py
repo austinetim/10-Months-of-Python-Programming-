a7ae111b641939031778b16a4d4c6d59a46f47d7
austinetim <austinetimothyfriday@gmail.com>
2026-01-11 23:56:48 +0100

    add reeborg's world step 10 robot control code

---
+++ month_2/reeborg_world_step_10.py
@@ -0,0 +1,22 @@
+# # Define the turn_right() function
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# #Using a function for the jumps
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#     move()
+
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
