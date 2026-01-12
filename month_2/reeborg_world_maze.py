de5ebf31017bc2b2410594f6b48e73e2bc13645e
austinetim <austinetimothyfriday@gmail.com>
2026-01-12 14:20:09 +0100

    add reeborg's world maze

---
+++ month_2/reeborg_world_maze.py
@@ -0,0 +1,17 @@
+# # This program instructs the robot to complete a maze
+# # Define the turn_right() function
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+    
+    
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif fron_is_clear():
+#         move()
+#     else:
+#         turn_left()
