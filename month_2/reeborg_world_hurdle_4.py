34e88c4218cd8c31eee10b068ed53d0858c5d347
austinetim <austinetimothyfriday@gmail.com>
2026-01-12 14:18:55 +0100

    add reeborg's world hurdle 4

---
+++ month_2/reeborg_world_hurdle_4.py
@@ -0,0 +1,25 @@
+# # This program instructs the robot to perform jumps over unequal bars
+
+# # Define the turn_right() function
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# #Using a function for the jumps
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move
+#     turn_left()
+
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
