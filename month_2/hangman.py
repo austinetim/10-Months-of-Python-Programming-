911ef78265f4e4a4ce91439e3c0a658112307ce3
austinetim <austinetimothyfriday@gmail.com>
2026-01-18 23:59:01 +0100

    add wrong and repeated letters check

--- month_2/hangman.py
+++ month_2/hangman.py
@@ -19,6 +19,9 @@ word_length = len(chosen_word)
 
 # Create blanks
 display = []
+
+
+
 for _ in range(word_length):
     display += "_"
 
@@ -33,6 +36,8 @@ while not end_of_game:
     #Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
     guess = input("Guess a letter: ").lower()
+    if guess in display:
+        print(f"You've already guessed {guess}")
 
     # Check guessed letter
     for position in range(word_length):
@@ -41,12 +46,13 @@ while not end_of_game:
         if letter == guess:
             display[position] = letter
     if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life")
         lives -= 1
         if lives == 0:
             end_of_game = True
             print("You lose!")
 
-    # print(f"{" ".join(display)}")
+    print(f"{" ".join(display)}")
     if "_" not in display:
         end_of_game = True
         print("You win!")
