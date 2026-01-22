f1a7312001e683800bac7e6c716e6fa95e9593b9
austinetim <austinetimothyfriday@gmail.com>
2026-01-22 23:56:20 +0100

    add the decrypt function.

--- month_3/caesar_cipher.py
+++ month_3/caesar_cipher.py
@@ -1,4 +1,4 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 
@@ -13,5 +13,19 @@ def encrypt(plain_text, shift_amount):
         new_letter = alphabet[new_position]
         cipher_text += new_letter
     print(f"The encoded text is {cipher_text}")
-    
-encrypt(plain_text=text, shift_amount=shift)
+
+# The decryption function:
+
+def decrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(f"The decoded text is {cipher_text}")
+
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+if direction == "decode":
+    decrypt(plain_text=text, shift_amount=shift)
