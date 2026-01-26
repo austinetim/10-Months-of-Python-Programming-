69d2ad25c0347e638a8cd1a3017e0eadd2c7c050
austinetim <austinetimothyfriday@gmail.com>
2026-01-26 23:28:51 +0100

    add shift and charater checker

--- month_3/caesar_cipher.py
+++ month_3/caesar_cipher.py
@@ -9,21 +9,19 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 
 text = input("Type the message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-
-# # Combining the encrypt and decrypt functions into a single function called 'caesar_cipher'
-
-
-
-#Another Method
+print(shift)
+shift = shift % 25
+print(shift)
 
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
-    for letter in start_text:
-        position = alphabet.index(letter)
+    for char in start_text:
         if cipher_direction == "decode":
             shift_amount *= -1
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
     print(f"The {cipher_direction}d text is {end_text}")
 caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
 
