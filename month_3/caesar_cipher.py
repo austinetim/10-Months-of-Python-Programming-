0a31bd64b8207c8728cfdc8303997484c50dc6dd
austinetim <austinetimothyfriday@gmail.com>
2026-01-27 23:40:03 +0100

    add while-loop controller

--- month_3/caesar_cipher.py
+++ month_3/caesar_cipher.py
@@ -4,60 +4,31 @@ print(logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-
-text = input("Type the message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-print(shift)
-shift = shift % 25
-print(shift)
-
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
-    for char in start_text:
-        if cipher_direction == "decode":
+    if cipher_direction == "decode":
             shift_amount *= -1
+    for char in start_text:
         if char in alphabet:
             position = alphabet.index(char)
             new_position = position + shift_amount
             end_text += alphabet[new_position]
+        else:
+             end_text += char
     print(f"The {cipher_direction}d text is {end_text}")
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type the message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 25
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
 
 
-
-
-
-
-
-
-
-
-
-# A different Approach
-
-
-# def caesar_cipher(plain_text, cipher_text, shift_amount):
-   
-#     if direction == "encode":
-#         cipher_text = ""
-#         for letter in plain_text:
-#             position = alphabet.index(letter)
-#             new_position = position + shift_amount
-#             new_letter = alphabet[new_position]
-#             cipher_text += new_letter
-#     print(f"The encoded text is {cipher_text}")
-
-    
-#     if direction == "decode":
-#         plain_text = ""
-#         for letter in cipher_text:
-#             position = alphabet.index(letter)
-#             new_position = position - shift_amount
-#             new_letter = alphabet[new_position]
-#             plain_text += new_letter
-#         print(f"The decoded text is {plain_text}")
-
-# # calling the fuction:
-# caesar_cipher(plain_text=text, cipher_text=text, shift_amount=shift)
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'\n")
+    if result == "no":
+        should_continue = False
+        print("Goodbye")
