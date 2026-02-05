eeca138878b1f8d573c0198164f45f5e307810d7
austinetim <austinetimothyfriday@gmail.com>
2026-02-05 23:40:57 +0100

    test the use cases of the return keyword

--- month_3/calculator.py
+++ month_3/calculator.py
@@ -33,6 +33,18 @@ operation_symbol = input("Pick an operation symbol from the line above: ")
 num2 = int(input("What's the second number?: "))
 
 calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
+answer_1 = calculation_function(num1, num2)
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+print(f"{num1} {operation_symbol} {num2} = {answer_1}")
+
+for operator in operations:
+    print(operator)
+
+operation_symbol = input("Pick an operation symbol from the line above: ")
+
+num3 = int(input("What the third number?: "))
+
+calculation_function = operations[operation_symbol]
+answer_2 = calculation_function(calculation_function(num1, num2), num2)
+
+print(f"{answer_1} {operation_symbol} {num3} = {answer_2}")
