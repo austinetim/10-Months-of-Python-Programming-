04a9523db351095f350fe87323af3120b7d909e6
austinetim <austinetimothyfriday@gmail.com>
2026-02-05 23:58:31 +0100

    add while loop for more control

--- month_3/calculator.py
+++ month_3/calculator.py
@@ -37,14 +37,17 @@ answer_1 = calculation_function(num1, num2)
 
 print(f"{num1} {operation_symbol} {num2} = {answer_1}")
 
-for operator in operations:
-    print(operator)
+continue_calculation = False
+while not continue_calculation:
+    check_continuation = input(f"Type 'y' to contine calculating with 8 or type 'n' to close")
+    for operator in operations:
+        print(operator)
+    if check_continuation == "y":
+        operation_symbol = input("Pick an operation symbol from the line above: ")
 
-operation_symbol = input("Pick an operation symbol from the line above: ")
+        num_n = int(input("What the next number?: "))
 
-num3 = int(input("What the third number?: "))
+        calculation_function = operations[operation_symbol]
+        answer_2 = calculation_function(calculation_function(num1, num2), num2)
 
-calculation_function = operations[operation_symbol]
-answer_2 = calculation_function(calculation_function(num1, num2), num2)
-
-print(f"{answer_1} {operation_symbol} {num3} = {answer_2}")
+        print(f"{answer_1} {operation_symbol} {num_n} = {answer_2}")
