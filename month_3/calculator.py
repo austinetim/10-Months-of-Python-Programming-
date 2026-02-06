b92474db7b754013e2e6778f4a5c88e2e1ee0cba
austinetim <austinetimothyfriday@gmail.com>
2026-02-06 21:36:04 +0100

    add more while loop control

--- month_3/calculator.py
+++ month_3/calculator.py
@@ -25,29 +25,20 @@ operations = {
 
 num1 = int(input("What's the first number?: "))
 
-for operator in operations:
-    print(operator)
 
-operation_symbol = input("Pick an operation symbol from the line above: ")
 
-num2 = int(input("What's the second number?: "))
-
-calculation_function = operations[operation_symbol]
-answer_1 = calculation_function(num1, num2)
-
-print(f"{num1} {operation_symbol} {num2} = {answer_1}")
-
-continue_calculation = False
+continue_calculation = True
 while not continue_calculation:
-    check_continuation = input(f"Type 'y' to contine calculating with 8 or type 'n' to close")
-    for operator in operations:
-        print(operator)
-    if check_continuation == "y":
-        operation_symbol = input("Pick an operation symbol from the line above: ")
+    operation_symbol = input("Pick an operation symbol from the line above: ")
 
-        num_n = int(input("What the next number?: "))
+    num2 = int(input("What's the next number?: "))
 
-        calculation_function = operations[operation_symbol]
-        answer_2 = calculation_function(calculation_function(num1, num2), num2)
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
 
-        print(f"{answer_1} {operation_symbol} {num_n} = {answer_2}")
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Type 'y' to contine calculating with {answer}, or type 'n' to exit: ") == "y":
+        num1 = answer
+    else:
+        continue_calculation = False
