6609a36d83cc2f3f7817a056483769bbde8f62ee
austinetim <austinetimothyfriday@gmail.com>
2026-02-08 23:59:35 +0100

    add blackjack init code

---
+++ month_3/blackjack.py
@@ -0,0 +1,18 @@
+###################### Black Jack ################
+import random
+def deal_card():
+    """Returns a random card from the deck"""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    #Pick a random card
+    card = random.choice(cards)
+    return card
+
+
+# Deal the user and computer 2 cards each using deal_card()
+
+user_cards = []
+computer_cards = []
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
