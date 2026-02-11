ed75af4fb342a76801dc7598e8f16a31f4c549f5
austinetim <austinetimothyfriday@gmail.com>
2026-02-11 23:17:34 +0100

    add more control on user score and computer score

--- month_3/blackjack.py
+++ month_3/blackjack.py
@@ -10,17 +10,38 @@ def deal_card():
 
 # Deal the user and computer 2 cards each using deal_card()
 
+
+
+def calculate_score(cards):
+    """Take a list of cards and return the score calculated from the cards"""
+
+
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.add(1)
+    return sum(cards)
+
 user_cards = []
 computer_cards = []
+is_game_over = False
 
 for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
-def calculate_score(cards):
 
-    if 11 in cards and 10 in cards and len(cards) == 2:
-        return 0
-
-
-    return sum(cards)
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+print(f"  Your cards: {user_cards}, Current score: {user_score}")
+print(f"  Computer's first cards: {computer_cards[0]}")
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
+else:
+    user_should_deal = input("Type 'y' to get another code, type 'n' to pass: ")
+    if user_should_deal == 'y':
+        user_cards.append(deal_card())
+    else: 
+        is_game_over = True
