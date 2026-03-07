03c91f440f1cf0ca973cea6b63a4116a14e0dacf
austinetim <austinetimothyfriday@gmail.com>
2026-03-07 21:40:29 +0100

    add MENU and Resources

--- month_4/coffee_machine/main.py
+++ month_4/coffee_machine/main.py
@@ -6,3 +6,35 @@ print("Hello World!")
 #3.  Process coins
 #4.  Check transaction successful?
 #5.  Make Coffee
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100
+}
