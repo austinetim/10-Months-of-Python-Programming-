23058d3c8d46702e3d124885747f74a0e2eefe5a
austinetim <austinetimothyfriday@gmail.com>
2026-03-07 22:29:55 +0100

    add choice and the off button

--- month_4/coffee_machine/main.py
+++ month_4/coffee_machine/main.py
@@ -1,3 +1,5 @@
+from random import choice
+
 print("Hello World!")
 
 # ------Program Requirements-----
@@ -38,3 +40,12 @@ resources = {
     "milk": 200,
     "coffee": 100
 }
+
+# TO_DO Tracking is a very handy feature of Python
+
+# Let the user be able to make their choice and the machine be turned off if need be.
+is_on = True
+while is_on:
+    choice = input("What would you like? espresso/latte/cappuccino: ")
+    if choice == "off":
+        is_on = False
