ed492cc26dda9448deb04bcf9a61531cf2a3ada7
austinetim <austinetimothyfriday@gmail.com>
2026-03-11 06:55:11 +0100

    add constructor/initializer of the object

--- month_5/main.py
+++ month_5/main.py
@@ -1,20 +1,24 @@
 # Creating a class --- the blueprint
 class User:
-    pass
+         def __init__(self, user_id, username):
+             self.id = user_id
+             self.username = username
+             self.followers = 0 # This is a default value. It does not need to be specified when the object is being created.
 
 #Creating an object from the blueprint
-user_1 = User()
+user_1 = User("001", "Clement")
+user_2 = User("002", "Timothy")
+print(user_1.id, user_1.username)
+
 
 #Adding attributes --- variable associated with an object
+# Without initialization:
 
-user_1.id = "001"
-user_1.username = "Clement"
+# user_1.id = "001"
+# user_1.username = "Clement"
+# print(user_1.username)
 
-print(user_1.username)
-
-user_2 = User()
-user_2.id = "002"
-user_2.username = "Tim"
-
-# CONSTRUCTORS
-# --- They allow us to specify what should happen when our objects is being created.
+# user_2 = User()
+# user_2.id = "002"
+# user_2.username = "Tim"
+# print(user_2.username)
