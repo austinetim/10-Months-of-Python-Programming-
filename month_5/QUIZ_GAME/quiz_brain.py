162f38612d6ba2dfed3e4013ffec3bba77b38ed4
austinetim <austinetimothyfriday@gmail.com>
2026-03-12 23:34:04 +0100

    add next_question method

--- month_5/QUIZ_GAME/quiz_brain.py
+++ month_5/QUIZ_GAME/quiz_brain.py
@@ -1,7 +1,9 @@
 # Restructured
-from main import question_bank
-
 class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.questions_list = q_list
+    def next_question(self):
+        current_question = self.questions_list[self.question_number]
+        self.question_number += 1
+        input(f"Q.{self.question_number}: {current_question.text} True/False?  ")
