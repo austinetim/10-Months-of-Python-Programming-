74545a866da8210c3702ef82353d9e0faab3f41f
austinetim <austinetimothyfriday@gmail.com>
2026-03-12 21:57:33 +0100

    add the quiz data

---
+++ month_5/QUIZ_GAME/data.py
@@ -0,0 +1,23 @@
+question_data = [
+    {"text": "Humans have feathers.", "answer": "False"},
+    {"text": "Water boils at 100 degrees Celsius at sea level.", "answer": "True"},
+    {"text": "The Great Wall of China is visible from space with the naked eye.", "answer": "False"},
+    {"text": "Venus is the hottest planet in our solar system.", "answer": "True"},
+    {"text": "Sharks are mammals.", "answer": "False"},
+    {"text": "Honey never spoils; archaeologists have found pots of honey in ancient"
+             " Egyptian tombs that are still edible.", "answer": "True"},
+    {"text": "The capital of Australia is Sydney.", "answer": "False"},
+    {"text": "Octopuses have three hearts.", "answer": "True"},
+    {"text": "Goldfish have a three-second memory.", "answer": "False"},
+    {"text": "Light travels faster than sound.", "answer": "True"},
+    {"text": "Bulls are enraged by the color red.", "answer": "False"},
+    {"text": "There are seven continents on Earth.", "answer": "True"},
+    {"text": "Bananas grow on trees.", "answer": "False"},
+    {"text": "A leap year has 366 days.", "answer": "True"},
+    {"text": "The human adult skeleton has 206 bones.", "answer": "True"},
+    {"text": "Bats are completely blind.", "answer": "False"},
+    {"text": "Mount Everest is the tallest mountain above sea level on Earth.", "answer": "True"},
+    {"text": "Napoleon Bonaparte was extremely short, standing at only 5 feet tall.", "answer": "False"},
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "The atomic number of oxygen is 8.", "answer": "True"}
+]
