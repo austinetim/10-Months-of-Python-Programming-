f369b94ad4bdb2a5f2c24afada4f394d4888b548
austinetim <austinetimothyfriday@gmail.com>
2026-03-13 23:36:03 +0100

    add question and correct_answer parameters

--- month_5/QUIZ_GAME/main.py
+++ month_5/QUIZ_GAME/main.py
@@ -3,8 +3,8 @@ from data import question_data
 from quiz_brain import QuizBrain
 question_bank = []
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(q_text=question_text, q_answer=question_answer)
     question_bank.append(new_question)
 
