e086fc9399e69ab583dd05e8793d46bb2a1bd9e7
austinetim <austinetimothyfriday@gmail.com>
2026-03-13 23:34:50 +0100

    add new data

--- month_5/QUIZ_GAME/data.py
+++ month_5/QUIZ_GAME/data.py
@@ -1,23 +1,127 @@
+# question_data = [
+#     {"text": "Humans have feathers.", "answer": "False"},
+#     {"text": "Water boils at 100 degrees Celsius at sea level.", "answer": "True"},
+#     {"text": "The Great Wall of China is visible from space with the naked eye.", "answer": "False"},
+#     {"text": "Venus is the hottest planet in our solar system.", "answer": "True"},
+#     {"text": "Sharks are mammals.", "answer": "False"},
+#     {"text": "Honey never spoils; archaeologists have found pots of honey in ancient"
+#              " Egyptian tombs that are still edible.", "answer": "True"},
+#     {"text": "The capital of Australia is Sydney.", "answer": "False"},
+#     {"text": "Octopuses have three hearts.", "answer": "True"},
+#     {"text": "Goldfish have a three-second memory.", "answer": "False"},
+#     {"text": "Light travels faster than sound.", "answer": "True"},
+#     {"text": "Bulls are enraged by the color red.", "answer": "False"},
+#     {"text": "There are seven continents on Earth.", "answer": "True"},
+#     {"text": "Bananas grow on trees.", "answer": "False"},
+#     {"text": "A leap year has 366 days.", "answer": "True"},
+#     {"text": "The human adult skeleton has 206 bones.", "answer": "True"},
+#     {"text": "Bats are completely blind.", "answer": "False"},
+#     {"text": "Mount Everest is the tallest mountain above sea level on Earth.", "answer": "True"},
+#     {"text": "Napoleon Bonaparte was extremely short, standing at only 5 feet tall.", "answer": "False"},
+#     {"text": "A slug's blood is green.", "answer": "True"},
+#     {"text": "The atomic number of oxygen is 8.", "answer": "True"}
+# ]
+
+
 question_data = [
-    {"text": "Humans have feathers.", "answer": "False"},
-    {"text": "Water boils at 100 degrees Celsius at sea level.", "answer": "True"},
-    {"text": "The Great Wall of China is visible from space with the naked eye.", "answer": "False"},
-    {"text": "Venus is the hottest planet in our solar system.", "answer": "True"},
-    {"text": "Sharks are mammals.", "answer": "False"},
-    {"text": "Honey never spoils; archaeologists have found pots of honey in ancient"
-             " Egyptian tombs that are still edible.", "answer": "True"},
-    {"text": "The capital of Australia is Sydney.", "answer": "False"},
-    {"text": "Octopuses have three hearts.", "answer": "True"},
-    {"text": "Goldfish have a three-second memory.", "answer": "False"},
-    {"text": "Light travels faster than sound.", "answer": "True"},
-    {"text": "Bulls are enraged by the color red.", "answer": "False"},
-    {"text": "There are seven continents on Earth.", "answer": "True"},
-    {"text": "Bananas grow on trees.", "answer": "False"},
-    {"text": "A leap year has 366 days.", "answer": "True"},
-    {"text": "The human adult skeleton has 206 bones.", "answer": "True"},
-    {"text": "Bats are completely blind.", "answer": "False"},
-    {"text": "Mount Everest is the tallest mountain above sea level on Earth.", "answer": "True"},
-    {"text": "Napoleon Bonaparte was extremely short, standing at only 5 feet tall.", "answer": "False"},
-    {"text": "A slug's blood is green.", "answer": "True"},
-    {"text": "The atomic number of oxygen is 8.", "answer": "True"}
-]
+    {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "Science: Computers",
+      "question": "To bypass US Munitions Export Laws, the creator of the PGP published all the source code in book form. ",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "Science: Computers",
+      "question": "A Boolean value of &quot;0&quot; represents which of these words?",
+      "correct_answer": "False",
+      "incorrect_answers": [
+        "True"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "Science: Computers",
+      "question": "The ARM computer chip was originally developed by Acorn Computers.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "Science: Computers",
+      "question": "Linus Sebastian is the creator of the Linux kernel, which went on to be used in Linux, Android, and Chrome OS.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+        "True"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "Science: Computers",
+      "question": "FLAC stands for &quot;Free Lossless Audio Condenser&quot;&#039;",
+      "correct_answer": "False",
+      "incorrect_answers": [
+        "True"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "Science: Computers",
+      "question": "It&#039;s not possible to format a write-protected DVD-R Hard Disk.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "Science: Computers",
+      "question": "The first dual-core CPU was the Intel Pentium D.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+        "True"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "Science: Computers",
+      "question": "Early RAM was directly seated onto the motherboard and could not be easily removed.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "Science: Computers",
+      "question": "Android versions are named in alphabetical order.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "Science: Computers",
+      "question": "The HTML5 standard was published in 2014.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    }
+  ]
