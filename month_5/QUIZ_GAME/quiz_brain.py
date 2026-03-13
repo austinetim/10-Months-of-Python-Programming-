02288b99f02505b58b1141eeb3adcc80aed03a21
austinetim <austinetimothyfriday@gmail.com>
2026-03-13 22:37:31 +0100

    add still has question method

--- month_5/QUIZ_GAME/quiz_brain.py
+++ month_5/QUIZ_GAME/quiz_brain.py
@@ -3,6 +3,10 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.questions_list = q_list
+
+    def still_has_questions(self):
+        return self.question_number < len(self.questions_list)
+
     def next_question(self):
         current_question = self.questions_list[self.question_number]
         self.question_number += 1
