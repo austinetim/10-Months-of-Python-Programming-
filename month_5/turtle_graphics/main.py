bc198d49ae9936d0e43b261ecb871bdcf9f5084b
austinetim <austinetimothyfriday@gmail.com>
2026-03-14 12:41:11 +0100

    add shape, color, movement and angle

---
+++ month_5/turtle_graphics/main.py
@@ -0,0 +1,31 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle") # This is an attribute
+timmy_the_turtle.color("red")
+timmy_the_turtle.forward(100) # This is a method
+timmy_the_turtle.right(90) # This is also a method
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
