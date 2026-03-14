ff18798b35c6423c856945e9b5583409fdca0bd1
austinetim <austinetimothyfriday@gmail.com>
2026-03-14 22:17:33 +0100

    add dashed lines drawn by turtle

--- month_5/turtle_graphics/main.py
+++ month_5/turtle_graphics/main.py
@@ -4,15 +4,20 @@ tim = Turtle()
 tim.shape("turtle") # This is an attribute
 tim.color("red")
 
-for _ in range(4):
-    tim.forward(100)  # This is a method
-    tim.right(90)
+# for _ in range(4):
+#     tim.forward(100)  # This is a method
+#     tim.right(90)
 # tim.forward(100)
 # tim.right(90)
 # tim.forward(100)
 # tim.right(90)
 # tim.forward(100)
 
+for _ in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
 
 
 
