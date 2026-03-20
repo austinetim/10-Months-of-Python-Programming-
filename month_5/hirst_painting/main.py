982afdf56b5c1e4fbcd23650fe28ca1cc52ab86f
austinetim <austinetimothyfriday@gmail.com>
2026-03-20 22:17:54 +0100

    Add Damien Hirst dot painting script

--- month_5/hirst_painting/main.py
+++ month_5/hirst_painting/main.py
@@ -1,4 +1,8 @@
-import colorgram
+import turtle as turtle_module
+import random
+#--------------------------------------------------------
+#--------------------------------------------------------
+# import colorgram
 
 # Extracting rgb colors from the image
 # rgb_colors = []
@@ -11,6 +15,36 @@ import colorgram
 #     rgb_colors.append(new_color)
 #
 # print(rgb_colors)
+#--------------------------------------------------------
+#--------------------------------------------------------
 
+
+turtle_module.colormode(255)
+tim = turtle_module.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
 # The generated rgb colors
-color_list = [(199, 175, 118), (125, 36, 24), (187, 158, 50), (170, 105, 56), (5, 57, 83), (222, 223, 226), (200, 216, 205), (108, 67, 85), (39, 36, 35), (86, 142, 58), (20, 123, 176), (110, 161, 175), (75, 39, 47), (9, 67, 47), (64, 153, 137), (133, 41, 43), (184, 98, 80), (179, 201, 186), (210, 200, 113), (179, 175, 177), (151, 176, 164), (93, 142, 156), (28, 80, 59), (194, 190, 192), (17, 78, 98), (213, 184, 173), (145, 116, 121), (176, 197, 202)]
+color_list = [(199, 175, 118), (125, 36, 24), (187, 158, 50), (170, 105, 56), (5, 57, 83), (222, 223, 226),
+              (200, 216, 205), (108, 67, 85), (39, 36, 35), (86, 142, 58), (20, 123, 176), (110, 161, 175),
+              (75, 39, 47), (9, 67, 47), (64, 153, 137), (133, 41, 43), (184, 98, 80), (179, 201, 186), (210, 200, 113),
+              (179, 175, 177), (151, 176, 164), (93, 142, 156), (28, 80, 59), (194, 190, 192), (17, 78, 98),
+              (213, 184, 173), (145, 116, 121), (176, 197, 202)]
+
+tim.setheading(255)
+tim.forward(300)
+tim.setheading(0)
+
+number_of_dots = 100
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+screen = turtle_module.Screen()
+screen.exitonclick()
