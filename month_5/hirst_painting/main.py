9846fcc56c763e3d254766b20bb9cd9f1ca53f3e
austinetim <austinetimothyfriday@gmail.com>
2026-03-20 06:30:02 +0100

    add generated rgb colors in a list

--- month_5/hirst_painting/main.py
+++ month_5/hirst_painting/main.py
@@ -1,13 +1,16 @@
 import colorgram
 
 # Extracting rgb colors from the image
-rgb_colors = []
-colors = colorgram.extract("image.jpg", 30)
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color = (r, g, b)
-    rgb_colors.append(new_color)
+# rgb_colors = []
+# colors = colorgram.extract("image.jpg", 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
 
-print(rgb_colors)
+# The generated rgb colors
+color_list = [(199, 175, 118), (125, 36, 24), (187, 158, 50), (170, 105, 56), (5, 57, 83), (222, 223, 226), (200, 216, 205), (108, 67, 85), (39, 36, 35), (86, 142, 58), (20, 123, 176), (110, 161, 175), (75, 39, 47), (9, 67, 47), (64, 153, 137), (133, 41, 43), (184, 98, 80), (179, 201, 186), (210, 200, 113), (179, 175, 177), (151, 176, 164), (93, 142, 156), (28, 80, 59), (194, 190, 192), (17, 78, 98), (213, 184, 173), (145, 116, 121), (176, 197, 202)]
