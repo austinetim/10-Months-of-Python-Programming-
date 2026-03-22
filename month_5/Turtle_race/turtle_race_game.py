d08ffc59c6b8cf9b64c1c0a4ea020179cd7c0b21
austinetim <austinetimothyfriday@gmail.com>
2026-03-22 23:14:52 +0100

    genetate the turtles

---
+++ month_5/Turtle_race/turtle_race_game.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+y_positions = [-100, -50, 0, 50, 100]
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.color(colors[turtle_index])
+    tim.penup()
+    tim.goto(x=-238, y=-100)
+
+
+
+screen.exitonclick()
