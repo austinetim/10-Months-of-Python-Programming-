3813668473877738b4e7d8900d0c86f4cd708e05
austinetim <austinetimothyfriday@gmail.com>
2026-04-09 13:56:58 +0100

    Add script to move paddle and update screen

--- month_6/pong-game/main.py
+++ month_6/pong-game/main.py
@@ -5,7 +5,7 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
-
+screen.tracer(0)
 
 paddle = Turtle("square")
 paddle.color("white")
@@ -13,9 +13,23 @@ paddle.shapesize(stretch_wid=5, stretch_len=1)
 paddle.penup()
 paddle.goto(350, 1)
 
+#Go up
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
 
+#Go Down
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
 
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
 
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 
 
