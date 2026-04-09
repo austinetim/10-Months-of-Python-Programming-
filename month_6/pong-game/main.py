0c60ef0988932ea6d805e80ca867dbd552c7cbf5
austinetim <austinetimothyfriday@gmail.com>
2026-04-09 13:44:14 +0100

    Create the paddle

--- month_6/pong-game/main.py
+++ month_6/pong-game/main.py
@@ -1,9 +1,17 @@
-from turtle import Screen
+from turtle import Turtle, Screen
+
+#Create the screen
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
 
+
+paddle = Turtle("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 1)
 
 
 
