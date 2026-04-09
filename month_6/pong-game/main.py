33741bb58e8c0c643c39f9fd9e712194c0cdb0df
austinetim <austinetimothyfriday@gmail.com>
2026-04-09 14:31:21 +0100

    Add the position init variable

--- month_6/pong-game/main.py
+++ month_6/pong-game/main.py
@@ -1,4 +1,5 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from paddle import Paddle
 
 #Create the screen
 screen = Screen()
@@ -7,26 +8,14 @@ screen.bgcolor("black")
 screen.title("Pong Game")
 screen.tracer(0)
 
-paddle = Turtle("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 1)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
-#Go up
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-#Go Down
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
-screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
-
+#
+# screen.listen()
+# screen.onkey(go_up, "Up")
+# screen.onkey(go_down, "Down")
+#
 game_is_on = True
 while game_is_on:
     screen.update()
