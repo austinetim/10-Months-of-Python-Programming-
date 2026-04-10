64298b9f96152c3ba3f2a8d59c0f3256d721d5d8
austinetim <austinetimothyfriday@gmail.com>
2026-04-10 23:50:23 +0100

    Add the move up and down key listeners

--- month_6/pong-game/main.py
+++ month_6/pong-game/main.py
@@ -6,15 +6,18 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
-# screen.tracer(0)
+#Disable the animation
+screen.tracer(0)
+#Move the two paddle to the left and right sides respectively
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
-#
-# screen.listen()
-# screen.onkey(go_up, "Up")
-# screen.onkey(go_down, "Down")
-#
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
 game_is_on = True
 while game_is_on:
     screen.update()
