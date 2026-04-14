920c025c9d99dc8aa4c05251198c017143dd3d06
austinetim <austinetimothyfriday@gmail.com>
2026-04-14 23:43:27 +0100

    Add the script to check collision with the paddle.

--- month_6/pong-game/main.py
+++ month_6/pong-game/main.py
@@ -29,7 +29,11 @@ while game_is_on:
 
     #Detect collision with the wall
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    #Detect collision with the paddle
+    if ball.distance(r_paddle) < 50 or ball.xcor() > 320 or ball.distance(l_paddle) < 50 or ball.xcor() > -320 :
+        ball.bounce_x()
 
 
 
