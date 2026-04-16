f310a32d856a9be109e045fc8de3143b1a549248
austinetim <austinetimothyfriday@gmail.com>
2026-04-17 00:00:07 +0100

    Increse left score upon paddle collision

--- month_6/pong-game/scoreboard.py
+++ month_6/pong-game/scoreboard.py
@@ -8,11 +8,16 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.l_score = 0
         self.r_score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
         self.goto(-100, 230)
         self.write(self.l_score, align="center", font=("courier", 50, "normal"))
         self.goto(100, 230)
         self.write(self.r_score, align="center", font=("courier", 50, "normal"))
 
-
+    def l_point(self):
+        self.l_score += 1
+        self.update_scoreboard()
 
 
