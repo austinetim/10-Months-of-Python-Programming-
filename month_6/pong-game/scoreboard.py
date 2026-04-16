438e00259114ad3de34b5185e29fecfda13c1733
austinetim <austinetimothyfriday@gmail.com>
2026-04-16 23:32:13 +0100

    Add right and left paddles scoreboard

--- month_6/pong-game/scoreboard.py
+++ month_6/pong-game/scoreboard.py
@@ -8,5 +8,11 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.l_score = 0
         self.r_score = 0
+        self.goto(-100, 230)
+        self.write(self.l_score, align="center", font=("courier", 50, "normal"))
+        self.goto(100, 230)
+        self.write(self.r_score, align="center", font=("courier", 50, "normal"))
+
+
 
 
